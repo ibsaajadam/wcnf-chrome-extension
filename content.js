9cb76ecda07510cc1dce6a0299322f05d69f7923
Ibsaa J. Adam <ibsaa.adam1@gmail.com>
2022-06-18 12:25:43 -0400

    added readme

--- content.js
+++ content.js
@@ -1,23 +1,23 @@
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse){
+    function(request, sender, sendResponse) {
         try {
-            document.getElementById("checkout_email_or_phone").value = request.email
-            document.getElementById("checkout_shipping_address_first_name").value = request.firstname
-            document.getElementById("checkout_shipping_address_last_name").value = request.lastname
-            document.getElementById("checkout_shipping_address_address1").value = request.address1
-            document.getElementById("checkout_shipping_address_address2").value = request.address2
-            document.getElementById("checkout_shipping_address_city").value = request.city
-            document.getElementById("checkout_shipping_address_country").value = request.country
-            document.getElementById("checkout_shipping_address_province").value = request.state
-            document.getElementById("checkout_shipping_address_zip").value = request.zip
-            document.getElementById("number").value = request.cardnumber
-            document.getElementById("name").value = request.cardname
-            document.getElementById("expiry").value = request.expiration
-            document.getElementById("verification_value").value = request.cvv
-            sendRespnse({status: "Success!"});
+            document.getElementById("checkout_email_or_phone").value = request.email;
+            document.getElementById("checkout_shipping_address_first_name").value = request.firstname;
+            document.getElementById("checkout_shipping_address_last_name").value = request.lastname;
+            document.getElementById("checkout_shipping_address_address1").value = request.address1;
+            document.getElementById("checkout_shipping_address_address2").value = request.address2;
+            document.getElementById("checkout_shipping_address_city").value = request.city;
+            document.getElementById("checkout_shipping_address_country").value = request.country;
+            document.getElementById("checkout_shipping_address_province").value = request.state;
+            document.getElementById("checkout_shipping_address_zip").value = request.zip;
+            document.getElementById("number").value = request.cardnumber;
+            document.getElementById("name").value = request.cardname;
+            document.getElementById("expiry").value = request.expiration; 
+            document.getElementById("verification_value").value = request.cvv; 
+            sendResponse({status: "Success!"});
         } catch (error) {
             console.log(error)
-            sendResponse({status: "Exception occured!"})
+            sendResponse({status: "Exception occurred!"});
         }
     }
-)
+);
--- ext/script.js
+++ ext/script.js
@@ -1,22 +1,90 @@
-document.getElementById("autofill").addEventListener("click", () => {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        chrome.tabs.sendMessage(tabs[0].id, {
-            email: document.getElementById('email').value,
-            firstname: document.getElementById('first-name').value,
-            lastname: document.getElementById('last-name').value,
-            address1: document.getElementById('address-1').value,
-            address2: document.getElementById('address-2').value,
-            city: document.getElementById('city').value,
-            country: document.getElementById('country').value,
-            state: document.getElementById('state').value,
-            zip: document.getElementById('zip').value,
-            cardnumber: document.getElementById('card-number').value,
-            cardname: document.getElementById('card-name').value,
-            expiration: document.getElementById('expiration').value,
-            cvv: document.getElementById('cvv').value,
-            
-        }, function (response){
-            console.log(response.status)
-        })
-    })
-})
+document.getElementById("auto-fill").addEventListener("click", () => {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {
+			email: document.getElementById('email').value,
+			firstname: document.getElementById('first-name').value,
+			lastname: document.getElementById('last-name').value,
+			address1: document.getElementById('address-1').value,
+			address2: document.getElementById('address-2').value,
+			city: document.getElementById('city').value,
+			country: document.getElementById('country').value,
+			state: document.getElementById('state').value,
+			zip: document.getElementById('zip').value,
+			cardnumber: document.getElementById('card-number').value,
+			cardname: document.getElementById('card-name').value,
+			expiration: document.getElementById('expiration').value,
+			cvv: document.getElementById('cvv').value
+		}, function(response) {
+			console.log(response.status);
+		});
+	});
+});
+
+document.getElementById("reset-fields").addEventListener("click", () => {
+	document.getElementById('email').value = '';
+	document.getElementById('firstname').value = '';
+	document.getElementById('lastname').value = '';
+	document.getElementById('address1').value = '';
+	document.getElementById('address2').value = '';
+	document.getElementById('city').value = '';
+	document.getElementById('country').value = '';
+	document.getElementById('state').value = '';
+	document.getElementById('zip').value = '';
+	document.getElementById('cardnumber').value = '';
+	document.getElementById('cardname').value = '';
+	document.getElementById('expiration').value = '';
+	document.getElementById('cvv').value = '';
+});
+
+
+document.getElementById("save").addEventListener("click", () => {
+	chrome.storage.sync.set({
+		email: document.getElementById('email').value,
+		firstname: document.getElementById('first-name').value,
+		lastname: document.getElementById('last-name').value,
+		address1: document.getElementById('address-1').value,
+		address2: document.getElementById('address-2').value,
+		city: document.getElementById('city').value,
+		country: document.getElementById('country').value,
+		state: document.getElementById('state').value,
+		zip: document.getElementById('zip').value,
+		cardnumber: document.getElementById('card-number').value,
+		cardname: document.getElementById('card-name').value,
+		expiration: document.getElementById('expiration').value,
+		cvv: document.getElementById('cvv').value
+	}, function() {
+		console.log("Saved!");
+	});
+});
+
+document.getElementById("load").addEventListener("click", () => {
+	chrome.storage.sync.get([
+		'email',
+		'firstname',
+		'lastname',
+		'address1',
+		'address2',
+		'city',
+		'country',
+		'state',
+		'zip',
+		'cardnumber',
+		'cardname',
+		'expiration',
+		'cvv'
+	], function(result) {
+		document.getElementById('email').value = result.email;
+		document.getElementById('first-name').value = result.firstname;
+		document.getElementById('last-name').value = result.lastname;
+		document.getElementById('address-1').value = result.address1;
+		document.getElementById('address-2').value = result.address2;
+		document.getElementById('city').value = result.city;
+		document.getElementById('country').value = result.country;
+		document.getElementById('state').value = result.state;
+		document.getElementById('zip').value = result.zip;
+		document.getElementById('card-number').value = result.cardnumber,
+		document.getElementById('card-name').value = result.cardname,
+		document.getElementById('expiration').value = result.expiration,
+		document.getElementById('cvv').value = result.cvv;
+	});
+});
